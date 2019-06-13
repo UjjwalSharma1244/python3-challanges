fdc4375f7253fba18fe44b085f2f938f263b3e60
ujjwal sharma <42812005+ujjusharma@users.noreply.github.com>
2019-06-13 18:21:15 +0000

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,10 @@
+import os
+import time
+from googlesearch import search
+web=input("enter topic t search ------->>>>" )
+url=[]
+for i in search(web,stop=10):
+	time.sleep(1)
+	url.append(i)
+	print(i)
+print(url)
